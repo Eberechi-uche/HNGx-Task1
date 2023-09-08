e219a907eba2962bc4a9fadae07af2e838ad84e6
Richard Eberechi Uche <eb3rechi@gmail.com>
2023-09-08 17:26:07 +0100

    add time card, javascript Date object and real time update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HNGx-Task1
-HNGx Task one
+
+HNGx Task one:
+used the javascript date object to retrieve the time
+used setInterval to update every one second.
+implemented a dynamic time card that changes with the time of the day.
+github link opens in new tab.
---
+++ index.js
@@ -0,0 +1,49 @@
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const greeting = ["Morning", "Afternoon", "Evening"];
+
+const daytime = ["&#x2615;", "&#x2600;&#xFE0F;", "&#x1F311;"];
+const date = new Date();
+const currentDay = date.getDay();
+const dayElement = document.getElementById("day");
+dayElement.innerHTML = days[currentDay];
+const daytimeIcon = document.getElementById("daytime-icon");
+const daytimeCard = document.getElementById("daytime-card");
+const timeElement = document.getElementById("time");
+
+function updateCurrentTime() {
+  const time = new Date();
+  const getHour = time.getHours();
+  const formatTime = time.toTimeString();
+
+  if (getHour >= 0 && getHour <= 11) {
+    daytimeIcon.innerHTML = daytime[0];
+    daytimeCard.classList.add("morning");
+    timeElement.innerHTML = formatTime.replace(
+      "GMT+0100 (West Africa Standard Time)",
+      "Good Afternoon! "
+    );
+  } else if (getHour >= 12 && getHour <= 18) {
+    daytimeIcon.innerHTML = daytime[1];
+    daytimeCard.classList.add("noon");
+    timeElement.innerHTML = formatTime.replace(
+      "GMT+0100 (West Africa Standard Time)",
+      "Good Afternoon! "
+    );
+  } else {
+    daytimeIcon.innerHTML = daytime[2];
+    daytimeCard.classList.add("evening");
+    timeElement.innerHTML = formatTime.replace(
+      "GMT+0100 (West Africa Standard Time)",
+      "Good Evening!"
+    );
+  }
+}
+setInterval(updateCurrentTime, 1000);
